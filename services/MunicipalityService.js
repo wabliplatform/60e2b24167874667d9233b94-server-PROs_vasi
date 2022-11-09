235eb9b808a4f01294fd76a25e1b2d442bc563d3
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-11-09 15:34:14 +0200

    Update MunicipalityService.js

--- services/MunicipalityService.js
+++ services/MunicipalityService.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
 const Service = require('./Service');
 const { Municipality } = require('../models/Municipality');
-const { pregetAllmunicipality } = require('../helperFunction');
 
 /**
 * Creates the data
@@ -53,7 +52,6 @@ const getAllmunicipality = () => new Promise(
     try {
       let query = {}
       query = await Municipality.find().populate(['mcandidates']).exec();
-      query = pregetAllmunicipality(query);
       // this is a test
       resolve(Service.successResponse(query));
     } catch (e) {
