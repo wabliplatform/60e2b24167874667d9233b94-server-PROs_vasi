ecd473348d22cadb9fa9bdd18b083cf9036eb4c8
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-11-11 13:41:03 +0200

    My commit message

--- helperFunction.js
+++ helperFunction.js
@@ -1,11 +1,7 @@
 /** This is a comment template
   * @param {string} paramName
-  * @return {Array} [Product]
+  * @return {Array} [Municipality]
   */
-function pregetAllmunicipality(param) {
+export function pregetAllmunicipality(param) {
   // insert code here
-  const f3param = [] ;
-  f3param.push(param[0])
-  return f3param;
 }
-module.export= {pregetAllmunicipality}
